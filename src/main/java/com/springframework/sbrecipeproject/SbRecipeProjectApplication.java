46d34f01e439176f1795c48eb6e50c30b23d636f
Andres Andrade <a.andrade09@yahoo.com>
2019-08-31 19:24:35 -0700

    Added Axis TCPMon plugin to project to monitor browser.

---
+++ src/main/java/com/springframework/sbrecipeproject/SbRecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.sbrecipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRecipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbRecipeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springframework/sbrecipeproject/SbRecipeProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springframework.sbrecipeproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbRecipeProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
